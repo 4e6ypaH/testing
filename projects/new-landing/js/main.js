743d23704ce66fe1ef498596b45977de40142915
Maxim Troshkov <maxitm_troshkov@mail.ru>
2019-07-09 16:38:52 +0300

    delete extra images and add calculation js

--- projects/new-landing/js/main.js
+++ projects/new-landing/js/main.js
@@ -10,3 +10,20 @@ for (let i=0; i < extraButtons.length; i++) {
 		extraButtons[i].style.display="none";
 		})
 }
+
+/* расчитываем стоимость в зависимости от введенного значения инпута*/
+let fixedSumArray =document.getElementsByClassName('fixed-sum');
+let oneHousePriceArray=document.getElementsByClassName('one-house-price');
+let resultArray =document.getElementsByClassName('result');
+let inputValueArray =document.querySelectorAll('input');
+
+
+/*после ввода значения в инпут изменяем значение блока result,
+ преобразовав слагаемые к числовому типу*/
+for (let i = 0; i < inputValueArray.length; i++) {
+	inputValueArray[i].addEventListener('keypress', function(){
+		resultArray[i].innerHTML = +fixedSumArray[i].innerHTML +
+		 +oneHousePriceArray[i].innerHTML*inputValueArray[i].value;
+		 console.log(resultArray[i].innerHTML);
+	})	
+}
