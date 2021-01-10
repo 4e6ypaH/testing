543454cebc01d8e1d8238ed3d19ac523a5538f87
4e6ypaH <smart22108@mail.ru>
2021-01-10 21:40:11 +0300

    Переписал логику для рандомайза, убрав текущего пользователя из списка.  Переписал js на es6. Поправил нейминг, добавил комментарии. Сделал форматирование.

--- projects/secret-santa/js/santa.js
+++ projects/secret-santa/js/santa.js
@@ -1,11 +1,11 @@
-var app = angular.module("app", ['ngStorage']);
+let app = angular.module("app", ['ngStorage']);
 
-app.controller("SecretSantaController", function($scope, $localStorage){
+app.controller("SecretSantaController", function ($scope, $localStorage) {
 
-    var uncheckedPerson = '';
+    let giftedPerson = '';
 
-    // заводим булки для отображения/скрытия нужных блоков на вьюхе
-    $scope.isUploaded = false; 
+    // заводим булки для отображения/скрытия нужных блоков на вьюхе (загрузка списка участников/получение учаcтника кому дарить)
+    $scope.isUploaded = false;
     $scope.isRecieved = false;
 
     getPersonsToView();
@@ -13,64 +13,65 @@ app.controller("SecretSantaController", function($scope, $localStorage){
     console.log($localStorage.personsStartedList);
     console.log($localStorage.persons);
 
-    // сохраняем список и его неизменную копию в локальном хранилище
-    $scope.savePersons = function() {
-            $localStorage.persons = $scope.enteredPersonsList.trim().split(',');
-            if ($localStorage.persons.length < 3) {
-                alert('Введите хотя бы 3 участников.');
-            } else {
-                $localStorage.personsStartedList = [];
-                var enteredPersonsList = angular.copy($scope.enteredPersonsList.trim().split(','));
-                enteredPersonsList.forEach( function(item){
-                    $localStorage.personsStartedList.push(item.trim());
-                });
-                console.log($localStorage.personsStartedList);
-                
-                $scope.isUploaded = true;
-                getPersonsToView(); 
-            }             
-    }
-    
-    // отображаем рандомного участника на вьюхе, проверив, что это не текущий пользователь
-    $scope.loadPerson = function() {
-        getUncheckedPerson();
-        checkPerson();     
-        $scope.isRecieved = true;
+    // сохраняем список участников и его неизменную копию в локальном хранилище
+    $scope.savePersons = () => {
+        $localStorage.persons = $scope.enteredPersonsList.trim().split(',');
+
+        if ($localStorage.persons.length < 3) {
+            alert('Введите хотя бы 3 участников.');
+            return;
         } 
 
-    $scope.reloadPage = function() {
-        location.reload()
-    }
+        $localStorage.personsStartedList = [];
+        let enteredPersonsList = angular.copy($scope.enteredPersonsList.trim().split(',')); // преобразуем введенную пользователем строку в массив
+        enteredPersonsList.forEach(item => {
+            $localStorage.personsStartedList.push(item.trim()); // наполняем массив для localStorage всеми введенными фамилиями, убирая лишние пробелы
+        });
+
+        $scope.isUploaded = true;
+        getPersonsToView();
         
+    }
+
+    // отображаем рандомного участника на вьюхе, проверив, что это не текущий пользователь
+    $scope.loadPerson = () => {
+        getGiftedPerson();
+        $scope.isRecieved = true;
+    }
+
+    $scope.reloadPage = () => location.reload();
+    
     // для корректного отображения блоков на вьюхе нужно дублировать код, поэтому вынес его в отдельную ф-ию 
     function getPersonsToView() {
-        $scope.persons = $localStorage.persons;
-        $scope.personsStartedList = $localStorage.personsStartedList;
+        $scope.persons = $localStorage.persons; // список оставшихся участников, у кого еще нет Санты 
+        $scope.personsStartedList = $localStorage.personsStartedList; // общий список участников для отображения на вьюхе
     }
 
-    function getUncheckedPerson() {
-        $scope.randomPersonIndex = Math.floor(Math.random() * $localStorage.persons.length);
-        uncheckedPerson = $localStorage.persons[$scope.randomPersonIndex].trim();  
-    }
-
-    function checkPerson() {
-        while (uncheckedPerson === $scope.user) {
-            if ($localStorage.persons.length === 1) {
-                alert('В списке осталась только ваша фамилия. Либо Вы уже стали Сантой, либо кто-то накосячил!');
-                location.reload();
-                return;
-            }
-            getUncheckedPerson();
+    function getGiftedPerson() {
+        // оповещаем пользователя, если в списке осталась только его фамилия
+        if ($localStorage.persons.length === 1 && $scope.user === $localStorage.persons[0].trim()) {
+            alert('В списке осталась только ваша фамилия. Либо Вы уже стали Сантой, либо кто-то накосячил!');
+            location.reload();
+            return;
         }
-        console.log($localStorage.personsStartedList);
-        var hasUser = $localStorage.personsStartedList.includes($scope.user);
+
+        // исключаем себя из списка при выборе участника
+        $scope.filteredPersons = $localStorage.persons.filter(item => item.trim() !== $scope.user);
+
+        $scope.randomPersonIndex = Math.floor(Math.random() * $scope.filteredPersons.length);
+        giftedPerson = $scope.filteredPersons[$scope.randomPersonIndex].trim();
+
+        let hasUser = $localStorage.personsStartedList.includes($scope.user);
+
         if (!hasUser) {
             alert('Введите корректную фамилию из списка');
             location.reload();
             return;
-        } 
+        }
 
-        $localStorage.persons.splice($scope.randomPersonIndex, 1);
-        $scope.checkedPerson = uncheckedPerson;
+        // исключаем выбранного участника из общего списка в localStorage
+        let indexToDelete = $localStorage.persons.findIndex(item => item.trim() === giftedPerson);
+        $localStorage.persons.splice(indexToDelete, 1);
+        $scope.giftedPerson = giftedPerson;
     }
 })
